8a30fd963c68ded7f11475d974878d9194f0b9bd
bruno-siqueira <bruno.siqueira@domo.com>
2022-07-18 16:32:48 -0300

    CRUD completed

--- node_sequelize/api/controllers/PersonController.js
+++ node_sequelize/api/controllers/PersonController.js
@@ -1,15 +1,69 @@
-import database from "../models/index.cjs"
+import database from "../models/index.cjs";
 
 class PersonController {
-    static getAll = async (req, res) => {
-        try {
-            const allPeople = await database.Persons.findAll()
-            return res.status(200).json(allPeople)
-        } catch (error) {
-            return res.status(500).json(error.message)
-        }
+  static getAll = async (req, res) => {
+    try {
+      const allPeople = await database.Persons.findAll();
+      return res.status(200).json(allPeople);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
+  };
 
+  static getById = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const person = await database.Persons.findOne({
+        where: { id: Number(id) },
+      });
+      if (person) {
+        return res.status(200).json(person);
+      } else {
+        return res.status(404).send("Not found");
+      }
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
+
+  static createPerson = async (req, res) => {
+    const newPerson = req.body;
+    try {
+      const person = await database.Persons.create(newPerson);
+      return res.status(201).json(person);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
+
+  static updatePerson = async (req, res) => {
+    const { id } = req.params;
+    const updatedPerson = req.body;
+    try {
+      await database.Persons.update(updatedPerson, {
+        where: { id: Number(id) },
+      });
+      const person = await database.Persons.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(person);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
+
+  static deletePerson = async (req, res) => {
+    const { id } = req.params;
+    try {
+      await database.Persons.destroy({
+        where: { id: Number(id) },
+      });
+      
+      return res.status(200).json("Deu boa");
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
 }
 
-export default PersonController
+export default PersonController;
--- node_sequelize/api/routes/personsRoute.js
+++ node_sequelize/api/routes/personsRoute.js
@@ -5,5 +5,9 @@ const router = Router()
 
 router
     .get("/people", PersonController.getAll)
+    .get("/people/:id", PersonController.getById)
+    .post("/people", PersonController.createPerson)
+    .put("/people/:id", PersonController.updatePerson)
+    .delete("/people/:id", PersonController.deletePerson)
 
 export default router
